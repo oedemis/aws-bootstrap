6841b9e2cc4f619e12d941eaf341f15799ac6f3f
Osman Recai Ödemis <osmanoedemis@gmx.de>
2022-10-04 01:51:45 +0200

    add lb

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -48,9 +48,13 @@ aws cloudformation deploy \
     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
+
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile awsbootstrap \
-    --query "Exports[?Name=='InstanceDNS'].Value"
+    --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
+  aws cloudformation list-exports \
+    --profile awsbootstrap \
+    --query "Exports[?starts_with(Name,'LBEndpoint')].Value"
 fi
