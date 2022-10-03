42e4d6c0fafd8e2130a4c49bae2179208d0febd7
Osman Recai Ödemis <osmanoedemis@gmx.de>
2022-10-04 01:08:33 +0200

    codepipeline/codedeploy/serverjs

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -42,6 +42,11 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
